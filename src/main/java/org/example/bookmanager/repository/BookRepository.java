e9f2e68b49f48af01977b3ff9f3cad43a333d870
mar <martorrijos@protonmail.com>
2025-06-14 16:51:48 +0200

    delete @Query tag

--- src/main/java/org/example/bookmanager/repository/BookRepository.java
+++ src/main/java/org/example/bookmanager/repository/BookRepository.java
@@ -2,17 +2,13 @@ package org.example.bookmanager.repository;
 
 import org.bson.types.ObjectId;
 import org.example.bookmanager.model.Book;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, ObjectId> {
 
-    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     Book findByTitle(String title);
-
-    @Query("{ 'author': { $regex: ?0, $options: 'i' } }")
     List<Book> findByAuthor(String author);
 
 }
